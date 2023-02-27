478bdec504256a5bb3c745819bc458b207a1cce2
Yolis28 <estradaryolanda@gmail.com>
2023-02-27 17:46:08 -0600

    Add workodoro function

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,41 +1,85 @@
-import React, { useEffect, useState } from "react";
+import React, { startTransition, useEffect, useState } from "react";
 import './timer.css'
 
 export const Timer = () => {
     const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
+    const [minutes, setMinutes] = useState(1);
     const [hours, setHours] = useState(0);
+    const [isCountdown, setIsCountdown] = useState(true);
+    const [toogleButtonText, setToogleButtonText] = useState('Work');
+    const [isPlaying, setIsPlaying] = useState(false);
 
     var timer;
 
     useEffect(() => {
-      timer = setInterval(() => {
-        setSeconds(seconds + 1);
+      if(isPlaying){
 
-        if(seconds===59){
-          setSeconds(0);
-          setMinutes(minutes + 1);
-          if(minutes === 59){
-            setMinutes(0);
-            setHours(hours + 1);
+        timer = setInterval(() => {
+          if (!isCountdown) {
+            setSeconds(seconds + 1);
+    
+            if(seconds===59){
+              setSeconds(0);
+              setMinutes(minutes + 1);
+              
+              if(minutes === 59){
+                setMinutes(0);
+                setHours(hours + 1);
+              }
+            }
           }
-        }
-        
-      }, 1000);
+  
+          if (isCountdown) {
+            if(seconds > 0) {
+              setSeconds(seconds - 1);
+            } else {
+              if(minutes > 0) {
+                setSeconds(59);
+                setMinutes(minutes - 1);
+              } else {
+                if (hours > 0) {
+                  setMinutes(59);
+                  setSeconds(59);
+                  setHours(hours - 1);
+                } else {
+                  start();
+                }
+              }
+            }
+          }
+          
+        }, 1000);
+      }
     
       return () => {
         clearInterval(timer);
       }
     });
     
-    const restart = () => {
-      setSeconds(0);
-      setMinutes(0);
-      setHours(0);
+
+    const start = () => {
+      setIsPlaying(true);
+      setIsCountdown(!isCountdown);
+      if(!isCountdown){
+        const time = seconds + (minutes * 60) + (hours * 3600);
+        const restTime = time * 0.32;
+        setHours(Math.floor(restTime / 3600));
+        setMinutes(Math.floor((restTime / 60) % 60));
+        setSeconds(Math.floor(restTime % 60));
+        setToogleButtonText('Work');
+      } else {
+        setToogleButtonText('Rest');
+      }
     }
 
     const stop = () => {
       clearInterval(timer);
+      setHours(0);
+      setMinutes(0);
+      setSeconds(0);
+      setIsPlaying(false);
+      setIsCountdown(true);
+      setToogleButtonText('Work');
     }
 
     return (
@@ -46,7 +90,7 @@ export const Timer = () => {
             <h1>
               {hours < 10 ? '0' + hours : hours}:{minutes < 10 ? '0' + minutes : minutes}:{seconds < 10 ? '0' + seconds : seconds}
             </h1>
-            <button className="restart" onClick={restart}>Restart</button>
+            <button className="restart" onClick={start}>{toogleButtonText}</button>
             <button className="stop" onClick={stop}>Stop</button>
           </div>
         </div>
